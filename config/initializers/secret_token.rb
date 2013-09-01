38839ce13898b7e6f3d87e10af8cd240db592c34
Ben Seligman <seligman.ben@gmail.com>
2013-09-01 12:38:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoteClipr::Application.config.secret_token = '22b20b813bbfcabb493603676b28ede340776cd1c81eace9a3557345004b0cae32b0ee18b67cf2ce25617cad6221042953da2738a33a5a59f51419823ccb1e65'
