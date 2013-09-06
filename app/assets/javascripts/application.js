c64226e9e3bf1afeb30675d4174908427e9ec45f
Ben Seligman <seligman.ben@gmail.com>
2013-09-05 18:02:39 -0700

    Note index shows time of last update.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.timeago
 //= require twitter/bootstrap
 //= require serializeJSON.min.js
 //= require underscore
--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,7 +1,7 @@
 class TaggingsController < ApplicationController
   def destroy
-    @tag = Tagging.find_by_note_id_and_tag_id(params[:note_id], params[:tag_id])
-    @tag.destroy
+    @tagging = Tagging.find_by_note_id_and_tag_id(params[:note_id], params[:tag_id])
+    @tagging.destroy
     render :json => @tag
   end
 end
