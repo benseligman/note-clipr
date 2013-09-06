d7a80179dc2dd4c857ea602ca3a1c1e482d5cbb4
Ben Seligman <seligman.ben@gmail.com>
2013-09-06 15:34:53 -0700

    Basic search functionality complete.

--- app/assets/javascripts/views/notes/notes_search.js
+++ app/assets/javascripts/views/notes/notes_search.js
@@ -1,16 +1,14 @@
 NoteClipr.Views.NotesSearch = Backbone.View.extend({
   initialize: function () {
-  },
-
-  events: {
-    "keyup": "applySearch"
+    // applies search every half-second
+    setInterval(this.applySearch.bind(this), 500);
   },
 
   template: JST['notes/search'],
 
   render: function () {
     this.$el.html(this.template());
-    // this.addTypeahead();
+    this.addTypeahead();
     return this;
   },
 
@@ -21,13 +19,23 @@ NoteClipr.Views.NotesSearch = Backbone.View.extend({
 
     noteTitles = _.compact(noteTitles);
 
-    this.$el.find(".search-query").typeahead({source: noteTitles});
+    var that = this;
+
+    this.$el.find(".search-query").typeahead({
+      source: noteTitles,
+      //apply search when typeahead is sorted. Doesn't refresh when bar is empty.
+      sorter: function (item) {
+        that.applySearch();
+        return item;
+      }
+    });
   },
 
   applySearch: function () {
-    var searchTerms = $("input.search-query").val();
+    var searchTerms = $("input.search-query").val(); //fix
+
     var that = this;
-    if (searchTerms) {
+    if (searchTerms.replace(/ /g, "")) {
       this.collection.each(function (note) {
         note.set("matchesSearch", that._checkMatch(note, searchTerms));
       });
@@ -41,7 +49,6 @@ NoteClipr.Views.NotesSearch = Backbone.View.extend({
   _checkMatch: function (note, searchTerms) {
     var re = new RegExp(".*" + searchTerms + ".*", "i");
     var title = note.get("title") || "";
-    return !!title.match(searchTerms);
+    return !!title.match(re);
   }
-
 });
